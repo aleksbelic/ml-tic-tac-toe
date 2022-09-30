d431d1c5a3a10e4773ea72c7ed103f9019ebf2ac
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-09-30 23:15:09 +0200

    Removing extra layers of code logic, leaving only features relevant for ML.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>ML Tic-Tac-Toe</h4>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
